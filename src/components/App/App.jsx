94fed54936f431126821ea6cc1f302ebbcb420fc
Max Logvinov <maxlogvinov93@gmail.com>
2025-04-08 03:20:18 +0300

    add landind page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,12 @@
 import Header from '../Header/Header';
 import './App.css';
+import Landing from './Landing/Landing';
 
 function App() {
   return (
     <div className="page">
       <Header />
+      <Landing />
     </div>
   );
 }
--- src/components/App/Landing/Landing.jsx
+++ src/components/App/Landing/Landing.jsx
@@ -1,7 +1,19 @@
 import './Landing.css';
 
 function Landing() {
-  return <div className="landing"></div>;
+  return (
+    <div className="landing">
+      <h1 className="landidng__title">
+        Создадим атмосферу <br /> уюта и комфорта <br /> в ваших пространствах
+      </h1>
+      <h2 className="landidng__heading">Все виды интерьерного озеленения</h2>
+      <h3 className="landidng__subheading">Создаем пространства, в которых хочется жить</h3>
+      <div className="landing__buttons">
+        <button className="landing__button">Оставить заявку</button>
+        <button className="landing__button landing__button_more">Узнать больше</button>
+      </div>
+    </div>
+  );
 }
 
 export default Landing;
